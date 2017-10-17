6b3c78c0a0cc380bb19945d032dee8696b5daa7a
johnNull <nullj27@gmail.com>
2017-10-16 21:15:19 -0400

    Added cases in MyLogger. Wrote Logger statements in Driver

--- airportSecurityState/src/airportSecurityState/driver/Driver.java
+++ airportSecurityState/src/airportSecurityState/driver/Driver.java
@@ -1,18 +1,36 @@
 package airportSecurityState.driver;
 import airportSecurityState.*;
+import airportSecurityState.util.MyLogger;
 
 public class Driver{
 	public static void main(String args[]){
 		if(args == null || args.length != 3)
 				throw new IllegalArgumentException("Three arguments are required, input.txt, output.txt, and debug level");
 		else{
+			try{
+				int debug = Integer.parseInt(args[2]);
+				if(debug > 0 && debug < 5)
+					MyLogger.setDebugValue(Integer.parseInt(args[2]));	
+				else
+					throw new IllegalArgumentException("debug level must be 0-4");
+			}
+			catch(Exception e){
+				System.err.println("Incorrect debug level format");
+				e.printStackTrace(System.out);
+			}
+			MyLogger.writeMessage("Creating FileProcessor", MyLogger.DebugLevel.IN_RESULTS);
 			FileProcessor fp = new FileProcessor(args[0]);
+			MyLogger.writeMessage("Creating Results instance", MyLogger.DebugLevel.IN_RESULTS);
 			Results results = new Results(args[1]);
+			MyLogger.writeMessage("Creating airport", MyLogger.DebugLevel.CONSTRUCTOR);
 			airport air = new airport();
+
+			MyLogger.writeMessage("Reading file", MyLogger.DebugLevel.IN_RESULTS);
 			for(String s = fp.readLine(); s != "-1"; s = fp.readLine()){
 				air.tightenOrLoosenSecurity(s);
 				air.operate(results);
 			}
+			MyLogger.writeMessage("Writing to file", MyLogger.DebugLevel.IN_RESULTS);
 			results.writeToFile();
 		}
 	}
--- airportSecurityState/src/airportSecurityState/util/MyLogger.java
+++ airportSecurityState/src/airportSecurityState/util/MyLogger.java
@@ -4,8 +4,8 @@ public class MyLogger{
 
     /*DEBUG_VALUE=4 [Print to stdout everytime a constructor is called]
       DEBUG_VALUE=3 [Print to stdout everytime the state is changed]
-      DEBUG_VALUE=2 [FIXME: add your own scheme here]
-      DEBUG_VALUE=1 [FIXME: add your own scheme here]
+      DEBUG_VALUE=2 [Print to stdout everytime a file operation occurs]
+      DEBUG_VALUE=1 [Print to stdout everytime a prohibited item is added]
       DEBUG_VALUE=0 [No output should be printed from the applicatio to stdout. It is ok to write to the output file though" ]
     */
 
@@ -17,9 +17,11 @@ public class MyLogger{
 
     public static void setDebugValue (int levelIn) {
 	switch (levelIn) {
-	  case 4: debugLevel = DebugLevel.CONSTRUCTOR; break;
-	      // FIXME: add code for other cases
-	  case 0: debugLevel = DebugLevel.RELEASE; break;
+	  	case 4: debugLevel = DebugLevel.CONSTRUCTOR; break;
+	  	case 3: debugLevel = DebugLevel.IN_RUN; break;
+		case 2: debugLevel = DebugLevel.IN_RESULTS; break;
+		case 1: debugLevel = DebugLevel.FROM_RESULTS; break;
+	  	case 0: debugLevel = DebugLevel.RELEASE; break;
 	}
     }
 
